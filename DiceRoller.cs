6711605b3862e05a5e01c75500b7a2941e71489f
Jimbobster123 <jimbo123@byu.edu>
2026-01-16 19:50:00 -0700

    Diceroller

--- DiceRoller.cs
+++ DiceRoller.cs
@@ -1,6 +1,25 @@
-namespace DiceThrowingSimulator;
+using System;
 
-public class DiceRoller
+namespace DiceThrowingSimulator
 {
-    
+    internal class DiceRoller
+    {
+        private readonly Random _rand = new Random();
+
+        public int[] RollDiceNTimes(int numberOfRolls)
+        {
+            int[] counts = new int[13];
+
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                int die1 = _rand.Next(1, 7);
+                int die2 = _rand.Next(1, 7);
+                int total = die1 + die2;
+
+                counts[total]++;
+            }
+
+            return counts;
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace DiceThrowingSimulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the dice throwing simulator!");
+            Console.Write("How many dice rolls would you like to simulate? ");
+
+            int rolls;
+            while (!int.TryParse(Console.ReadLine(), out rolls) || rolls <= 0)
+            {
+                Console.Write("Please enter a positive whole number: ");
+            }
+
+            DiceRoller roller = new DiceRoller();
+            int[] results = roller.RollDiceNTimes(rolls);
+
+            Console.WriteLine();
+            Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+            Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+            Console.WriteLine($"Total number of rolls = {rolls}.");
+            Console.WriteLine();
+
+            for (int total = 2; total <= 12; total++)
+            {
+                double percent = (results[total] / (double)rolls) * 100;
+                int stars = (int)Math.Round(percent);
+
+                Console.Write($"{total}: ");
+                Console.WriteLine(new string('*', stars));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+        }
+    }
+}
+
